2d3a680fda2cbd7f0d134beb27f4dcb24d9d6664
simple-clinic <almuntheri.wesam@yahoo.com>
2026-09-10 17:24:14 +0300

    Use simplevetclinic.win in owner messages

--- lib/clinic-links.ts
+++ lib/clinic-links.ts
@@ -1,4 +1,4 @@
-export const OWNER_PORTAL_URL = "https://simple-vet-clinic.workers.dev";
+export const OWNER_PORTAL_URL = "https://simplevetclinic.win";
 export const CLINIC_INSTAGRAM_URL = "";
 export const STORE_INSTAGRAM_URL = "";
 
