fec9f9dd6ea3900aab896511effed4726828b5dc
simple-clinic <almuntheri.wesam@yahoo.com>
2026-09-10 17:32:10 +0300

    Add Simple Vet social links to messages

--- lib/clinic-links.ts
+++ lib/clinic-links.ts
@@ -1,5 +1,8 @@
 export const OWNER_PORTAL_URL = "https://simplevetclinic.win";
-export const CLINIC_INSTAGRAM_URL = "";
+export const CLINIC_INSTAGRAM_URL = "https://www.instagram.com/simple.vet.clinic";
+export const CLINIC_TIKTOK_URL = "https://www.tiktok.com/@simple.clinic";
 export const STORE_INSTAGRAM_URL = "";
 
-export const WHATSAPP_SOCIAL_LINKS = "Simple Vet Clinic";
+export const WHATSAPP_SOCIAL_LINKS = `تابعوا Simple Vet Clinic:
+${CLINIC_INSTAGRAM_URL}
+${CLINIC_TIKTOK_URL}`;
